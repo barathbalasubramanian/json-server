efee4c9748125ccb9f1b94fa319f521a897d895a
BarathKumar B <105224717+barathbalasubramanian@users.noreply.github.com>
2023-02-17 21:00:05 +0530

    Update server.js

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ const jsonServer = require('json-server');
 const server = jsonServer.create();
 const router = jsonServer.router('model.json');
 const middlewares = jsonServer.defaults();
-const port = 8080;
+const port = 3000;
 
 server.use(middlewares);
 server.use(router);
